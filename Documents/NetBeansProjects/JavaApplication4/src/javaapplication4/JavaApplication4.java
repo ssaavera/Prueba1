e207141f4e8f4db343135ae32e3856865b3c976f
DAM119 <DAM119@C1115.iesataulfo.local>
2020-10-08 18:07:47 +0200

    Prueba

--- Documents/NetBeansProjects/JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ Documents/NetBeansProjects/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -17,7 +17,7 @@ public class JavaApplication4 {
     public static void main(String[] args) {
         // TODO code application logic here
     
-    System.out.println("Prueba 2");
+    System.out.println("Prueba 3");
     
     }
     
